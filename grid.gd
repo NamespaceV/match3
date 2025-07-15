3eca857ab2747fe5fb6c0adf00026bf4667c4141
NamespaceV <s-zephyr@o2.pl>
2025-07-15 09:56:51 +0200

    swap tiles

--- grid.gd
+++ grid.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var grid = []
+var selected_tile : Tile
 
 func _ready() -> void:
 	fill_grid()
@@ -12,6 +13,9 @@ func fill_grid():
 		for y in 10:
 			var randomType = randi_range(0,6) as Tile.TileType
 			var t = Tile.create(randomType)
+			t.on_tile_clicked.connect(on_tile_clicked)
+			t.x = x
+			t.y = y
 			add_child(t)
 			grid[x].append(t)
 			t.position = tile_position(x,y)
@@ -22,3 +26,33 @@ func tile_position(x,y):
 func reset():
 	print("reset")
 	get_tree().change_scene_to_file("res://game.tscn")
+
+func on_tile_clicked(tile:Tile):
+	if selected_tile == null:
+		selected_tile = tile
+		tile.set_selected(true)
+		return
+
+	if tile == selected_tile:
+		return
+
+	if tile.is_near(selected_tile):
+		var t2 = selected_tile
+		selected_tile.set_selected(false)
+		selected_tile = null
+		swap_tiles(t2, tile)
+	else:
+		selected_tile.set_selected(false)
+		selected_tile = tile
+		tile.set_selected(true)
+
+func swap_tiles(t1:Tile, t2:Tile):
+	var t1_xy = Vector2i(t1.x, t1.y)
+	t1.x = t2.x
+	t1.y = t2.y
+	t2.x = t1_xy[0]
+	t2.y = t1_xy[1]
+	t1.position = tile_position(t1.x, t1.y)
+	t2.position = tile_position(t2.x, t2.y)
+	grid[t1.x][t1.y] = t1
+	grid[t2.x][t2.y] = t2
--- tile/tile.gd
+++ tile/tile.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 static var scene : PackedScene = load("res://tile/tile.tscn")
 
+signal on_tile_clicked( tile : Tile)
+
 enum TileType
 {
 	red,
@@ -14,6 +16,8 @@ enum TileType
 }
 
 var type : TileType
+var x:int
+var y:int
 
 static func create(type:TileType) -> Tile:
 	var o = scene.instantiate()
@@ -34,3 +38,21 @@ func get_color(type:TileType) -> Color:
 		TileType.violet: return Color.VIOLET
 	push_error("color not found %s" % [type])
 	return Color.WHITE
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			on_tile_clicked.emit(self)
+
+func set_selected(selected:bool)->void:
+	if selected:
+		$Selection.show()
+	else :
+		$Selection.hide()
+
+func is_near(other:Tile)->bool:
+	if x == other.x:
+		return abs(y-other.y) == 1
+	if y == other.y:
+		return abs(x-other.x) == 1
+	return false
